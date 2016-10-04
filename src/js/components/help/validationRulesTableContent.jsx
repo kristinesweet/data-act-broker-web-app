94a857927682cca5fa3b0d0e80ef2bb0e5df5137
Kevin Li <li_kevin@bah.com>
2016-10-04 17:22:25 -0400

    Converted to ES6

--- src/js/components/help/validationRulesTableContent.jsx
+++ src/js/components/help/validationRulesTableContent.jsx
@@ -79,14 +79,14 @@ export default class ValidationRulesTableContent extends React.Component {
 				  data={this.state.data} filterable={['Rule Detail']}
 				  sortable={[
 					  {
-						  column:'Rule Name', sortFunction: function(a,b){
-							var reA = /[^a-zA-Z]/g;
-							var reN = /[^0-9]/g;
-						    var aA = a.replace(reA, "");
-						    var bA = b.replace(reA, "");
-						    if(aA === bA) {
-						        var aN = parseInt(a.replace(reN, ""), 10);
-						        var bN = parseInt(b.replace(reN, ""), 10);
+						  column:'Rule Name', sortFunction: (a,b) => {
+							const reA = /[^a-zA-Z]/g;
+							const reN = /[^0-9]/g;
+						    const aA = a.replace(reA, "");
+						    const bA = b.replace(reA, "");
+						    if (aA === bA) {
+						        const aN = parseInt(a.replace(reN, ""), 10);
+						        const bN = parseInt(b.replace(reN, ""), 10);
 						        return aN === bN ? 0 : aN > bN ? 1 : -1;
 						    } else {
 						        return aA > bA ? 1 : -1;
